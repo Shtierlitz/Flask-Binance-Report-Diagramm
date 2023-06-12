1e9b95af76d2627322912d426c3e18b8cde13538
Shtierlitz <rollbar1990@gmail.com>
2023-06-12 21:05:09 +0300

    Managed to set up Celery and Beat. I am finalizing templates and rendering functions.

--- project/celery_maker.py
+++ project/celery_maker.py
@@ -1,10 +1,23 @@
+# project/celery_maker.py
+
 from celery import Celery
+from datetime import timedelta
 
 celery = Celery(__name__, broker='redis://localhost:6379/0', backend='redis://localhost:6379/0')
+celery.conf.beat_schedule = {
+        'collect_data_every_minute': {
+            'task': 'project.tasks.task_collect_data',
+            'schedule': timedelta(seconds=30),  # запуск каждую минуту
+            # Здесь вы можете передать аргументы в вашу задачу, если они необходимы
+            'args': ('hour', '1', '1h')  # замените на ваши аргументы
+        },
+    }
+
 
 def init_celery(app):
     celery.conf.broker_url = app.config['CELERY_BROKER_URL']
     celery.conf.result_backend = app.config['CELERY_RESULT_BACKEND']
+
 
     celery.conf.update(app.config)
 
